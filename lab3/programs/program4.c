f924c757552092c96b5143299e47c64b95a680a1
Mohammed-Nour <o.shaheen1882@gmail.com>
2025-03-15 00:25:28 +0300

    lab3(program4): hotfix program4

--- lab3/programs/program4.c
+++ lab3/programs/program4.c
@@ -1,14 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char* getString() {
-    char* ret = "Hello World!";
+    char* ret = malloc(100);  
+    if (ret == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    strncpy(ret, "Hello World!", 99);  
+    ret[99] = '\0';
     return ret;
 }
 
 void program4() {
-    printf("String: %s\n", getString());
+    char* str = getString();
+    printf("String: %s\n", str);
+    free(str);
 }
 
 int main() {
